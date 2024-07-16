31f6e8c5f60acc9015cafd54ff95118046bd6059
reneSchm <49305466+reneSchm@users.noreply.github.com>
2024-07-16 12:48:25 +0200

    move TDPF to its own header

--- cpp/models/abm/parameters.h
+++ cpp/models/abm/parameters.h
@@ -27,7 +27,7 @@
 #include "memilio/config.h"
 #include "memilio/io/auto_serialize.h"
 #include "memilio/io/io.h"
-#include "memilio/math/interpolation.h"
+#include "memilio/math/time_dependent_parameter_functor.h"
 #include "memilio/utils/custom_index_array.h"
 #include "memilio/utils/uncertain_value.h"
 #include "memilio/utils/parameter_set.h"
@@ -35,10 +35,8 @@
 #include "memilio/epidemiology/damping.h"
 #include "memilio/epidemiology/contact_matrix.h"
 #include <algorithm>
-#include <cassert>
 #include <limits>
 #include <utility>
-#include <vector>
 
 namespace mio
 {
@@ -295,76 +293,6 @@ struct AerosolTransmissionRates {
     {
         return "AerosolTransmissionRates";
     }
-};
-
-class TimeDependentParameterFunctor
-{
-public:
-    enum class Type
-    {
-        Zero,
-        LinearInterpolation,
-    };
-
-    using DataType = std::vector<std::vector<ScalarType>>;
-    TimeDependentParameterFunctor(Type type, DataType data)
-        : m_type(type)
-        , m_data(data)
-    {
-        // data preprocessing
-        switch (m_type) {
-        case Type::Zero:
-            // no preprocessing needed
-            break;
-        case Type::LinearInterpolation:
-            // make sure data has the correct shape, i.e. a list of (time, value) pairs
-            assert(m_data.size() > 0);
-            assert(std::all_of(m_data.begin(), m_data.end(), [](auto&& a) {
-                return a.size() == 2;
-            }));
-            // sort by time
-            std::sort(m_data.begin(), m_data.end(), [](auto&& a, auto&& b) {
-                return a[0] < b[0];
-            });
-        }
-    }
-
-    TimeDependentParameterFunctor()
-        : TimeDependentParameterFunctor(Type::Zero, {})
-    {
-    }
-
-    ScalarType operator()(ScalarType time) const
-    {
-        switch (m_type) {
-        case Type::Zero:
-            return 0.0;
-        case Type::LinearInterpolation:
-            // find next time point in m_data (strictly) after time
-            const auto next_tp = std::upper_bound(m_data.begin(), m_data.end(), time, [](auto&& t, auto&& tp) {
-                return t < tp[0];
-            });
-            if (next_tp == m_data.begin()) { // time is before first data point
-                return m_data.front()[1];
-            }
-            if (next_tp == m_data.end()) { // time is past last data point
-                return m_data.back()[1];
-            }
-            const auto tp = next_tp - 1;
-            return linear_interpolation(time, (*tp)[0], (*next_tp)[0], (*tp)[1], (*next_tp)[1]);
-        }
-
-        return 0.0; // should be unreachable, but without this the compiler may complain about a missing return.
-    }
-
-    auto auto_serialize()
-    {
-        return make_auto_serialization("TimeDependentParameterFunctor", NVP("type", m_type), NVP("data", m_data));
-    }
-
-private:
-    Type m_type;
-    DataType m_data;
 };
 
 // using InputFunctionForProtectionLevel = std::function<ScalarType(ScalarType)>;
--- cpp/simulations/abm.cpp
+++ cpp/simulations/abm.cpp
@@ -472,7 +472,7 @@ void set_parameters(mio::abm::Parameters params)
 
     // Set protection level from high viral load. Information based on: https://doi.org/10.1093/cid/ciaa886
     params.get<mio::abm::HighViralLoadProtectionFactor>() = {
-        mio::abm::TimeDependentParameterFunctor::Type::LinearInterpolation,
+        mio::TimeDependentParameterFunctor::Type::LinearInterpolation,
         {{0, 0.863}, {1, 0.969}, {7, 0.029}, {10, 0.002}, {14, 0.0014}, {21, 0}}};
 
     //0-4
